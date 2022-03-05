26bbd4108e427a8ec2281ca4f0ad52e94081ef06
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-03-05 19:48:29 +0000

    css added to desktop upload page

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -5,28 +5,39 @@ export default function UploadPage() {
   return (
     <>
       <h1 className=" Upload__main-title">Upload Video</h1>
+
       <form className="Upload__form" /*onSubmit={handleSubmit}*/>
-        <div className="Upload__form-section">
-          <label className="Upload__subtitle">UPLOAD THUMBNAIL</label>
-          <img src={Thumbnail} alt="thumbnail" className="Upload__thumbnail" />
-        </div>
-        <div className="Upload__form-section">
-          <label className="Upload__subtitle">TITLE YOUR VIDEO</label>
-          <input
-            className="Upload__input-title"
-            type="text"
-            name="title"
-            placeholder="Add a title to your video"
-          />
-        </div>
-        <div className="Upload__form-section">
-          <label className="Upload__subtitle">ADD A VIDEO DESCRIPTION</label>
-          <input
-            className="Upload__input-description"
-            type="text"
-            name="description"
-            placeholder="Add a description to your video"
-          />
+        <div className="Upload__desktop-whole-container">
+          <div className="Upload__form-section">
+            <label className="Upload__subtitle">UPLOAD THUMBNAIL</label>
+            <img
+              src={Thumbnail}
+              alt="thumbnail"
+              className="Upload__thumbnail"
+            />
+          </div>
+          <div className="Upload__desktop-input-container">
+            <div className="Upload__form-section">
+              <label className="Upload__subtitle">TITLE YOUR VIDEO</label>
+              <input
+                className="Upload__input-title"
+                type="text"
+                name="title"
+                placeholder="Add a title to your video"
+              />
+            </div>
+            <div className="Upload__form-section">
+              <label className="Upload__subtitle">
+                ADD A VIDEO DESCRIPTION
+              </label>
+              <input
+                className="Upload__input-description"
+                type="text"
+                name="description"
+                placeholder="Add a description to your video"
+              />
+            </div>
+          </div>
         </div>
         <div className="Upload__button-container">
           <button className="Upload__publish-button" type="submit">
